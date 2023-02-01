1d0ee3f1439d411a7fcf57c04287c80dcfee9c22
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2023-02-01 15:53:28 +0200

    fixup! ASoC: SOF: tokens: add SOF_TKN_COMP_PAYLOAD_WITH_OUTPUT_FMT
    Remove the token (should drop the whole patch at the end)
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- include/uapi/sound/sof/tokens.h
+++ include/uapi/sound/sof/tokens.h
@@ -132,7 +132,6 @@
 
 /* Processing Components */
 #define SOF_TKN_PROCESS_TYPE                    900
-#define SOF_TKN_PROCESS_PAYLOAD_WITH_OUTPUT_FMT 901
 
 /* for backward compatibility */
 #define SOF_TKN_EFFECT_TYPE	SOF_TKN_PROCESS_TYPE
