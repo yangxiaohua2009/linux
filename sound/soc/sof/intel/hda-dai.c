81709ed326d4de8088987a78fd763231fd63cf60
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2023-04-19 16:57:55 -0500

    ASoC: SOF: Intel: hda-dai: mark functions as __maybe_unused
    hda_dai_hw_params, hda_dai_trigger(), hda_dai_hw_free are currently
    only used for HDaudio codec support, but will be reused for
    SSP/DMIC/SoundWire in the LunarLake/ACE2.x case. To avoid 'defined but
    not used' errors or added complexity in Kconfig, mark all these
    functions as __maybe_used.
    
    When SSP/DMIC/SoundWire are added, some of these changes may be
    reverted. For now this avoids compilation warnings.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/hda-dai.c
+++ sound/soc/sof/intel/hda-dai.c
@@ -190,7 +190,8 @@ static int hda_link_dma_hw_params(struct snd_pcm_substream *substream,
 	return 0;
 }
 
-static int hda_dai_hw_free(struct snd_pcm_substream *substream, struct snd_soc_dai *cpu_dai)
+static int __maybe_unused hda_dai_hw_free(struct snd_pcm_substream *substream,
+					  struct snd_soc_dai *cpu_dai)
 {
 	const struct hda_dai_widget_dma_ops *ops = hda_dai_get_ops(substream, cpu_dai);
 	struct hdac_ext_stream *hext_stream;
@@ -208,9 +209,9 @@ static int hda_dai_hw_free(struct snd_pcm_substream *substream, struct snd_soc_d
 	return hda_link_dma_cleanup(substream, hext_stream, cpu_dai);
 }
 
-static int hda_dai_hw_params(struct snd_pcm_substream *substream,
-			     struct snd_pcm_hw_params *params,
-			     struct snd_soc_dai *dai)
+static int __maybe_unused hda_dai_hw_params(struct snd_pcm_substream *substream,
+					    struct snd_pcm_hw_params *params,
+					    struct snd_soc_dai *dai)
 {
 	struct snd_soc_dapm_widget *w = snd_soc_dai_get_widget(dai, substream->stream);
 	const struct hda_dai_widget_dma_ops *ops = hda_dai_get_ops(substream, dai);
@@ -245,7 +246,8 @@ static int hda_dai_hw_params(struct snd_pcm_substream *substream,
  * In contrast to IPC3, the dai trigger in IPC4 mixes pipeline state changes
  * (over IPC channel) and DMA state change (direct host register changes).
  */
-static int hda_dai_trigger(struct snd_pcm_substream *substream, int cmd, struct snd_soc_dai *dai)
+static int __maybe_unused hda_dai_trigger(struct snd_pcm_substream *substream, int cmd,
+					  struct snd_soc_dai *dai)
 {
 	const struct hda_dai_widget_dma_ops *ops = hda_dai_get_ops(substream, dai);
 	struct hdac_ext_stream *hext_stream;
