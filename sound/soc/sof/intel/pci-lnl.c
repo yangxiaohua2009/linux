2dcff5929a0b7592c06736e4e341de27dce0eda4
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2023-07-31 17:02:08 -0500

    fixup! ASoC: SOF: Intel: LNL: Add support for Lunarlake platform
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/pci-lnl.c
+++ sound/soc/sof/intel/pci-lnl.c
@@ -48,8 +48,7 @@ static const struct sof_dev_desc lnl_desc = {
 
 /* PCI IDs */
 static const struct pci_device_id sof_pci_ids[] = {
-	{ PCI_DEVICE(0x8086, 0xa828), /* LNL */
-		.driver_data = (unsigned long)&lnl_desc},
+	{ PCI_DEVICE_DATA(INTEL, HDA_LNL_P, &lnl_desc) }, /* LNL-P */
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, sof_pci_ids);
