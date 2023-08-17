7f9512a31dc2a3737f1a78ab410d4d2c6ca6b8b2
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2023-08-17 15:01:04 -0500

    ASoC: SOF: Intel: hda-dai-ops: fix HDaudio link format
    The generic format calculation is shared between SSP, DMIC and
    SoundWire.
    
    In the latter case, the dailink may handle more channels than what
    each DAI can handle. This is specifically the case for aggregated
    configurations, specifically for capture where we may want to handle a
    4ch stream using two DAIs.
    
    The issue is that soc-pcm.c already splits the channels on a per-dai
    basis, so we need to count the total number of channels handled by the
    dailink. This isn't very elegant but the information on the dailink
    configuration is not available at the DAI level.
    
    The net effect of this confusion between DAI and dailink channels was
    that the data was recorded 2x slower than normal, and when played at
    the normal rate a 2x frequency shift can be heard.
    
    Fixes: 12547730e5b7 ("ASoC: SOF: Intel: hda-dai-ops: add/select DMA ops for SSP")
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/hda-dai-ops.c
+++ sound/soc/sof/intel/hda-dai-ops.c
@@ -237,15 +237,32 @@ static unsigned int generic_calc_stream_format(struct snd_sof_dev *sdev,
 					       struct snd_pcm_substream *substream,
 					       struct snd_pcm_hw_params *params)
 {
+	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
+	struct snd_soc_dai *codec_dai;
 	unsigned int format_val;
+	u32 ch_mask = 0;
+	int num_channels;
+	int codec_dai_id;
 
-	format_val = snd_hdac_calc_stream_format(params_rate(params), params_channels(params),
+	/*
+	 * if the multiple dais are handled by the same dailink, we may need to update the
+	 * stream channel count - the params are modified in soc-pcm based on the codec_ch_maps info
+	 */
+	for_each_rtd_codec_dais(rtd, codec_dai_id, codec_dai)
+		ch_mask |= rtd->dai_link->codec_ch_maps[codec_dai_id].ch_mask;
+
+	num_channels = hweight_long(ch_mask);
+	if (num_channels != params_channels(params))
+		dev_dbg(sdev->dev, "configuring stream format for %d channels, params_channels was %d\n",
+			num_channels, params_channels(params));
+
+	format_val = snd_hdac_calc_stream_format(params_rate(params), num_channels,
 						 params_format(params),
 						 params_physical_width(params),
 						 0);
 
 	dev_dbg(sdev->dev, "format_val=%#x, rate=%d, ch=%d, format=%d\n", format_val,
-		params_rate(params), params_channels(params), params_format(params));
+		params_rate(params), num_channels, params_format(params));
 
 	return format_val;
 }
