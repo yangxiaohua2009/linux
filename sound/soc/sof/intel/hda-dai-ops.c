9917e64d0228fe6a318f07247578755ec48ab4e1
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2023-10-10 11:09:13 -0400

    fixup! ASoC: SOF: Intel: hda-dai-ops: fix HDaudio link format
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/hda-dai-ops.c
+++ sound/soc/sof/intel/hda-dai-ops.c
@@ -237,7 +237,7 @@ static unsigned int generic_calc_stream_format(struct snd_sof_dev *sdev,
 					       struct snd_pcm_substream *substream,
 					       struct snd_pcm_hw_params *params)
 {
-	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
+	struct snd_soc_pcm_runtime *rtd = snd_soc_substream_to_rtd(substream);
 	struct snd_soc_dai *codec_dai;
 	unsigned int format_val;
 	u32 ch_mask = 0;
