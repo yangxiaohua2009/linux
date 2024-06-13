7924899a503e02d5b6ec68cf66e4155e603a17c0
Bard Liao <yung-chuan.liao@linux.intel.com>
2024-06-13 09:24:12 +0800

    ASoC: Intel: soc-acpi-intel-ptl-match: add rt722 l3 adr
    Add rt722 support on SoundWire link 3.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- sound/soc/intel/common/soc-acpi-intel-ptl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-ptl-match.c
@@ -65,11 +65,29 @@ static const struct snd_soc_acpi_adr_device rt722_0_single_adr[] = {
 	}
 };
 
+static const struct snd_soc_acpi_adr_device rt722_3_single_adr[] = {
+	{
+		.adr = 0x000330025d072201ull,
+		.num_endpoints = ARRAY_SIZE(rt722_endpoints),
+		.endpoints = rt722_endpoints,
+		.name_prefix = "rt722"
+	}
+};
+
 static const struct snd_soc_acpi_link_adr ptl_rt722_only[] = {
 	{
 		.mask = BIT(0),
 		.num_adr = ARRAY_SIZE(rt722_0_single_adr),
 		.adr_d = rt722_0_single_adr,
+	},
+	{}
+};
+
+static const struct snd_soc_acpi_link_adr ptl_rt722_l3[] = {
+	{
+		.mask = BIT(3),
+		.num_adr = ARRAY_SIZE(rt722_3_single_adr),
+		.adr_d = rt722_3_single_adr,
 	},
 	{}
 };
@@ -115,6 +133,12 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_ptl_sdw_machines[] = {
 		.links = ptl_rt722_only,
 		.drv_name = "sof_sdw",
 		.sof_tplg_filename = "sof-ptl-rt722-l0.tplg",
+	},
+	{
+		.link_mask = BIT(3),
+		.links = ptl_rt722_l3,
+		.drv_name = "sof_sdw",
+		.sof_tplg_filename = "sof-ptl-rt722-l3.tplg",
 	},
 	{},
 };
